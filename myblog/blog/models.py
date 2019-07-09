a3f677704b24913dafa81603aa991b65af92faa3
KGITBank <KGITBank@404-116>
2019-07-09 14:54:32 +0900

    1차 업로드

---
+++ myblog/blog/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+from django.utils import timezone
+
+
+class Post(models.Model):
+    #author는 사용자의 createsuperuser혹은 superuser가 만든 계정자료와
+    #연동, 글쓴이는 자동으로 로그인한 사용자의 정보가 기입된다.
+    author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    #CharField는 글자수 제한이 있는 컬럼을 의미, max_length로길이를 제한
+    title = models.CharField(max_length=200)
+    #TextField는 글자수 제한이 없는 컬럼을 의미
+    text = models.TextField()
+    #DateTimeField는 시간 저장을 위한 컬럼을 의미, defalut는 입력하지 않을때
+    #자동으로 가입할 자료를 정해줌. timezone.now는 등록 당시의 서버시간
+    created_date = models.DateTimeField(
+            default=timezone.now)
+    #blank=True일 경우 컬럼이 비어있어도 됨. null=True는 null 값을 넣기 허용
+    published_date = models.DateTimeField(
+            blank=True, null=True)
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.title
